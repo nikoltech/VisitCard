14e557739ee4be3d4c02b58690affc567b64a75c
Vitali Nikolenko <nvvnikolenko@gmail.com>
2020-05-14 19:10:10 +0300

    Configured Multi-level architecture

---
+++ VisitCardApp.DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VisitCardApp.DataAccess.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ VisitCardApp.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VisitCardApp.DataAccess
+{
+    public class Class1
+    {
+    }
+}
